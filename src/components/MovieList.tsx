29840d74a8c22cc042c1a8758753f8e4cc22d75a
pittmand <pr0grammer@hotmail.com>
2023-03-20 19:26:28 -0400

    Add components to list movies

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,16 +1,24 @@
 import './MovieList.css'
+import Movie from '../model/Movie';
+import MovieListItem from './MovieListItem';
+import { useEffect, useState } from "react";
+import { getTopRatedMovies } from '../services/movieApiService';
 
-interface Props {
-  movies: [];
-}
 
-const MovieList = ({ movies }: Props) => {
+const MovieList = () => {
+
+  const [movie, setMovie] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    getTopRatedMovies().then((res) => setMovie(res.results));
+  }, []);
+
   return (
     <div className="MovieList">
       <h2>Results</h2>
       <ul>
-        {movies.map((item) => (
-          <Result key={item.id + Math.random()} gif={item} />
+        {movie.map((item) => (
+          <MovieListItem key={item.id + Math.random()} movie={item} />
         ))}
       </ul>
     </div>
---
+++ src/components/MovieListItem.tsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import Movie from '../model/Movie';
+import './MovieListItem.css'
+
+interface Props {
+    movie: Movie;
+  }
+  
+
+const MovieListItem = ({ movie }: Props) => {
+  return (
+    <div className='MovieListItem'>
+      <li className="Result">
+            <Link to={`/gif/${movie.id}`}>
+              <h3>{movie.title}</h3>
+              {/* <img src={`https://image.tmdb.org/t/p/original/`+movie.poster_path} alt="the GIF" /> */}
+            </Link>
+            {/* <p className=".link">
+              <a href={movie.} target="_blank" rel="noreferrer">
+                Link to Giphy
+              </a>
+            </p> */}
+          </li>
+
+
+    </div>
+
+
+
+
+
+
+  )
+};
+
+export default MovieListItem;
