587b74cad41d4e71a5b34aa0c4337102eeea79f1
Bikale Aberra <bikaleg@gmail.com>
2023-03-22 19:51:31 -0400

    fix infinite call

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,31 +1,37 @@
 //import './GenreList.css'
-import { useEffect, useState } from 'react';
-import Genre from '../model/Genre';
-import { getGenreLists } from '../services/movieApiService';
+import { useEffect, useState } from "react";
+import Genre from "../model/Genre";
+import { getGenreLists } from "../services/movieApiService";
 
 const GenreList = () => {
-    const [genre, setGenre] =  useState<Genre[]>([]);
-                             //useState<Movie[]>([]);
-    useEffect(() => {
-        getGenreLists().then((res) => {
-          setGenre(res.genres)
-          console.log("res.data", res.genres)
-        });
-      }, [genre]);
-      
+  const [genre, setGenre] = useState<Genre[]>([]);
+  //useState<Movie[]>([]);
+  useEffect(() => {
+    getGenreLists().then((res) => {
+      setGenre(res.genres);
+      console.log("res.data", res.genres);
+    });
+  }, []);
+
   return (
-    <div className='GenreList'>
-       <div>
-          <select>
-            {genre ? genre.map((genre) => {
-                console.log("download list", genre)
-                return <option key={genre.id} value={genre.id}>{genre.name}</option>
-            })
-            :""};
-          </select>
-        </div>
+    <div className="GenreList">
+      <div>
+        <select>
+          {genre
+            ? genre.map((genre) => {
+                console.log("download list", genre);
+                return (
+                  <option key={genre.id} value={genre.id}>
+                    {genre.name}
+                  </option>
+                );
+              })
+            : ""}
+          ;
+        </select>
+      </div>
     </div>
-  )
+  );
 };
 
 export default GenreList;
